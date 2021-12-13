c15c4acf1d82586053a8f80170ed5b65d43ef345
jabed37 <jabed.aleshasolutions@gmail.com>
2021-12-13 10:22:08 +0600

    git add

---
+++ src/component/app.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import './app.style.css'
+
+class App extends React.Component{
+    render(){
+        return <h1>Lorem ipsum is testing</h1>
+    }
+}
+
+export default App;
